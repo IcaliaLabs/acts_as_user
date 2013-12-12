c1f57815d25e9f6f8a1ae0222bfcf88893a5c8ce
Abraham Kuri <kurenn@icalialabs.com>
2013-12-11 18:15:42 -0600

    Adds userable columns to the ignored attributes array

--- lib/acts_as_user.rb
+++ lib/acts_as_user.rb
@@ -1,10 +1,7 @@
 require "acts_as_user/version"
 
 module ActsAsUser
-  @@default_ignored_attributes = ["created_at", "updated_at", "id"]
-
-  mattr_accessor :devise
-  @@devise = defined?(Devise) 
+  @@default_ignored_attributes = ["created_at", "updated_at", "id", "userable_type", "userable_id"]
 
   mattr_accessor :ignored_attributes
   @@ignored_attributes = @@ignored_attributes.to_a + @@default_ignored_attributes
@@ -13,4 +10,10 @@ module ActsAsUser
     yield self    
   end
 
+  def self.devise?
+    defined?(Devise) 
+  end
 end
+
+require 'acts_as_user/user_delegate'
+require 'acts_as_user/railtie'
