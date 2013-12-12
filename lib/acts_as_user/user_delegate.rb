eadaefc4468f1d7ab627d15941f07601c5a95d02
Abraham Kuri <kurenn@icalialabs.com>
2013-12-11 18:15:54 -0600

    Adds delegate user methods module

---
+++ lib/acts_as_user/user_delegate.rb
@@ -0,0 +1,62 @@
+module ActsAsUser
+  module UserDelegate
+    def self.included(base)
+      base.has_one :user, :as => :userable, :dependent => :destroy, :autosave => true
+      base.validate :user_must_be_valid
+      base.alias_method_chain :user, :autobuild
+      base.extend ClassMethods
+      base.define_user_accessors
+    end
+
+    def user_with_autobuild
+      user_without_autobuild || build_user
+    end
+
+    def method_missing(meth, *args, &blk)
+      user.send(meth, *args, &blk)
+    rescue NoMethodError
+      super
+    end
+
+    protected
+
+    def user_must_be_valid
+      unless user.valid?
+        user.errors.each do |attr, message|
+          errors.add(attr, message)
+        end
+      end
+    end
+
+    module ClassMethods
+
+      def define_user_accessors
+        all_attributes = User.columns.map(&:name)
+
+        if ActsAsUser.devise?
+          all_attributes << "password"
+          all_attributes << "password_confirmation"
+          ActsAsUser.ignored_attributes << "encrypted_password"
+        end
+
+        attributes_to_delegate = all_attributes - ActsAsUser.ignored_attributes
+
+        attributes_to_delegate.each do |attrib|
+          class_eval <<-RUBY
+          def #{attrib}
+            user.#{attrib}
+          end
+
+          def #{attrib}=(value)
+            self.user.#{attrib} = value
+          end
+
+          def #{attrib}?
+            self.user.#{attrib}?
+          end
+            RUBY
+        end
+      end
+    end
+  end
+end
