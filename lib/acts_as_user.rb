3fd0e6c228a88ca4eaebfa438a6c5e323276617e
Abraham Kuri <kurenn@icalialabs.com>
2014-01-14 11:36:13 -0600

    Eager loads the acts as user user delegate and is user module to improve load time

--- lib/acts_as_user.rb
+++ lib/acts_as_user.rb
@@ -2,19 +2,46 @@ require "acts_as_user/version"
 require 'orm_adapter'
 
 module ActsAsUser
+  extend ActiveSupport::Autoload
+
+  #Eager loads the modules
+  eager_autoload do
+    autoload :UserDelegate
+    autoload :IsUser
+  end
+
+  # ActiveSupport::Autoload automatically defines
+  # an eager_load! method. In this case, we are
+  # extending the method to also eager load the
+  # ActsAsUser modules.
+  def self.eager_load!
+    super 
+    ActsAsUser::UserDelegate.eager_load!
+    ActsAsUser::IsUser.eager_load!
+  end
+
+  #We ignore some attribues that might cause a collision between models
   @@default_ignored_attributes = ["created_at", "updated_at", "id", "userable_type", "userable_id"]
 
+  #Array to define the models that are inhering from the user
+  @@models_acting_like_users = []
+  mattr_reader :models_acting_like_users
+
+  #We append the extra attributes you want to ignore to the default ones
   mattr_accessor :ignored_attributes
   @@ignored_attributes = @@ignored_attributes.to_a + @@default_ignored_attributes
+
 
   def self.setup
     yield self    
   end
 
+  #Checking if devise is present
   def self.devise?
     defined?(Devise).present?
   end
 
+  #We add some virtual attributes that dont't play well when devise is present
   def self.add_devise_attributes_to_ignore
     if self.devise?
       devise_ignore_attrs = ['password', 'password_confirmation', 'encrypted_password']
@@ -22,8 +49,13 @@ module ActsAsUser
       self.ignored_attributes.flatten!
     end
   end
+
+  private
+
+    def self.append_model_acting_like_user(class_name = "")
+      ActsAsUser.models_acting_like_users << class_name 
+    end
+  
 end
 
-require 'acts_as_user/user_delegate'
-require 'acts_as_user/is_user'
 require 'acts_as_user/railtie'
--- lib/acts_as_user/railtie.rb
+++ lib/acts_as_user/railtie.rb
@@ -1,15 +1,19 @@
 module ActsAsUser
   class Railtie < Rails::Railtie
-    ActiveSupport.on_load :active_record do
-      ActsAsUser.add_devise_attributes_to_ignore
-      class ActiveRecord::Base
-        def self.acts_as_user
-          include ActsAsUser::UserDelegate    
-        end
-        def self.is_user
-          include ActsAsUser::IsUser
+      #Adds namespace for eager loading
+      config.eager_load_namespaces << ActsAsUser
+
+      ActiveSupport.on_load :active_record do
+        ActsAsUser.add_devise_attributes_to_ignore
+
+        class ActiveRecord::Base
+          def self.acts_as_user
+            include ActsAsUser::UserDelegate    
+          end
+          def self.is_user
+            include ActsAsUser::IsUser
+          end
         end
       end
-    end
   end
 end
